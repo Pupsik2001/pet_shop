b70ac62c07a1544a5879db4fdd2f2efd5c9973db
Pupsik2001 <kenguru13422431@gmail.com>
2024-01-22 15:24:10 +0600

    load the entire template in teh original form

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "shopkg" SUPERUSER;
+CREATE DATABASE "shopkg" OWNER "shopkg" ENCODING 'utf-8';
